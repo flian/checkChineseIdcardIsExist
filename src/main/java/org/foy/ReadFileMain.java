4f85f3704a04a9668f98fd45cac92ccd597841d7
foy <ljs2342003@gmail.com>
2017-06-20 09:15:27 +0800

    add io compare

--- src/main/java/org/foy/ReadFileMain.java
+++ src/main/java/org/foy/ReadFileMain.java
@@ -16,7 +16,7 @@ import java.util.List;
  */
 public class ReadFileMain {
     public static void main(String[] args) throws IOException {
-        buildMappedByteBuffer();
+        //buildMappedByteBuffer();
         build();
 
     }
